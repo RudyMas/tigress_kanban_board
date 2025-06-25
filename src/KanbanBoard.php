c6362531f5fefbc724ed31c7fd29357948f42bfb
RudyMasGoNext <rudy.mas@go-next.be>
2025-06-25 11:16:20 +0200

    Renamed class

--- src/KanbanBoard.php
+++ src/KanbanBoard.php
@@ -3,15 +3,15 @@
 namespace Tigress;
 
 /**
- * Class KanbanBoards (PHP version 8.4)
+ * Class KanbanBoard (PHP version 8.4)
  *
  * @author Rudy Mas <rudy.mas@rudymas.be>
  * @copyright 2025 Rudy Mas (https://rudymas.be)
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
  * @version 2025.06.25.3
- * @package Controller\KanbanController
+ * @package Controller\KanbanBoard
  */
-class KanbanBoards
+class KanbanBoard
 {
     /**
      * @return string
